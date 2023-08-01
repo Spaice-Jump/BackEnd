d289ec17cd0646c8ba50870e8dc06a43d4718426
Jesus martin romero <jesusmartinromero10@gmail.com>
2023-08-01 10:40:33 +0200

    avanzamos con el login

--- controller/loginController.js
+++ controller/loginController.js
@@ -5,18 +5,24 @@ const bcrypt = require('bcrypt');
 
 class LoginController {
 
-    index(req, res, next){
-        res.render("login");
-    }
+    index(req, res, next) {
+        res.locals.error = '';
+        res.locals.email = '';
+        res.render('login')
+      }
 
     async Authenticate(req, res, next){
         try {
             const {email, password} = req.body;
-    
+            
+            //busca en la BD si coincide el email del usuario
             const userData = await User.findOne({email: email});
 
+            
+            //si no encuentro o no coincide la contraseña dar el error
             if(!userData || !(await bcrypt.compare(password, userData.password))){
                 res.locals.error = 'El usuario no existe';
+                res.locals.email= email;
                 res.redirect('/login');
                 return;
             }
