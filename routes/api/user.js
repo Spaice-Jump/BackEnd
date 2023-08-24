0f4e203eae74201c9f8b140886c5474d7fb0469f
Cristian Varela Casas <cristianchantada@gmail.com>
2023-08-24 16:20:38 +0200

    Reparando EditTravel

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,100 +2,90 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/users');
 
-
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
 //GET default response API
 router.post('/', function (req, res, next) {
-  res.json({ API: 'Good Day In Space' });
+	res.json({ API: 'Good Day In Space' });
 });
-
-
 
 //POST /api/v1/signup (body)
 // Create un User
 router.post(
-  '/signup',
-  upload.array('files'),
+	'/signup',
+	upload.array('files'),
 
-  async function (req, res, next) {
-    try {
-      const user = req.body.user;
-      const email = req.body.email;
-      const password = req.body.password; 
-    
-      if (email.indexOf('@') === -1) {
-        res.json({ status: 400, message : "Email is not valid"  }); 
-        return
-      }
+	async function (req, res, next) {
+		try {
+			const user = req.body.user;
+			const email = req.body.email;
+			const password = req.body.password;
 
-      //Habilitate Crypto Password
-      const newUser = new User({user:user , email:email, password:await  User.hashPassword(password) });
-      //const newUser = new User({user:user , email:email, password:password });
+			if (email.indexOf('@') === -1) {
+				res.json({ status: 400, message: 'Email is not valid' });
+				return;
+			}
 
+			//Habilitate Crypto Password
+			const newUser = new User({
+				user: user,
+				email: email,
+				password: await User.hashPassword(password),
+			});
+			//const newUser = new User({user:user , email:email, password:password });
 
-      const userSave = await newUser.save();
+			const userSave = await newUser.save();
 
-      res.json({ status:"OK", result: userSave });
-      return
-    } catch (error) {
-        
-        if (error.code===11000){  
-            res.json({ status: 400, message : "User duplicate"  }); 
-            return
-        }else{
-            res.json({ status: 400, message : "Error Create User"  }); 
-            return
-        }
-      
-    }
-  }
+			res.json({ status: 'OK', result: userSave });
+			return;
+		} catch (error) {
+			if (error.code === 11000) {
+				res.json({ status: 400, message: 'User duplicate' });
+				return;
+			} else {
+				res.json({ status: 400, message: 'Error Create User' });
+				return;
+			}
+		}
+	}
 );
 
 // Borrar un User
 
+router.post(
+	'/deleteuser',
+	upload.array('files'),
 
-router.post('/deleteuser',
-upload.array('files'),
+	async function (req, res, next) {
+		try {
+			const userId = req.body.userId;
+			const email = req.body.email;
+			const password = req.body.password;
 
-async function (req, res, next) {
-  try {
+			if (email.indexOf('@') === -1) {
+				res.json({ status: 400, message: 'Email is not valid' });
+				return;
+			}
 
+			// buscar el usuario en la BD
+			const user = await User.findOne({ email: email });
 
-    const userId = req.body.userId;
-    const email = req.body.email;
-    const password = req.body.password; 
+			// si no lo encuentro o no coincide la contraseña --> error
+			if (!user || !(await user.comparePassword(password))) {
+				res.json({ status: 400, error: 'invalid credentials' });
 
-  
-  
-    if (email.indexOf('@') === -1) {
-      res.json({ status: 400, message : "Email is not valid"  }); 
-      return
-    }
+				return;
+			}
 
-     // buscar el usuario en la BD
-     const user = await User.findOne({ email: email });
+			await User.deleteOne(user);
 
-    
-     // si no lo encuentro o no coincide la contraseña --> error
-     if (!user || !(await user.comparePassword(password))) {
-       
-       res.json({ status: 400, error: 'invalid credentials' });
-
-       return;
-     }
-    
-    await User.deleteOne(user)
-
-    res.json({ status:"OK", result: email });
-    return
-  } catch (error) {
-      
-          res.json({ status: 400, message : "Error Delete User"  }); 
-          return   
-  }
-}
-
+			res.json({ status: 'OK', result: email });
+			return;
+		} catch (error) {
+			res.json({ status: 400, message: 'Error Delete User' });
+			return;
+		}
+	}
 );
 module.exports = router;
