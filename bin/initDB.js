d6fbfaf653036de272a000f2fb3b3ddeb3bb8f7f
Xavi Roca <xroca@vilamatica.com>
2023-08-25 12:41:00 +0200

    Inserción 3 anuncios con el id del usuario demo@gmail.com

--- bin/initDB.js
+++ bin/initDB.js
@@ -2,8 +2,12 @@
 
 var app = require('../app');
 
+// const bcrypt = require('bcrypt');
+
 const Travel = require('../models/Travels');
 const Location = require('../models/locations');
+const User = require('../models/users');
+
 const connection = require('../lib/connectMongoose');
 
 
@@ -13,34 +17,36 @@ main().catch(err => console.log('***There was an error***',err));
 
 async function main() {
 
+    const newUserId = await initUser();
+
     await initLocations();
 
-    await initTravels();
+    await initTravels(newUserId);
 
     connection.close();
 }
 
-async function initTravels() {
+async function initUser() {
 
-    // Deleting previous travels
-    const deleted = await Travel.deleteMany();
-    console.log(`***Deleted ${deleted.deletedCount} travels.***`);
+    // Deleting previous users
+    const deleted = await User.deleteMany();
+    console.log(`***Deleted ${deleted.deletedCount} users.***`);
 
-    // New travels creation
-    const inserted = await Travel.insertMany ([
+    // New users creation
+    const inserted = await User.insertMany ([
         {
-            topic:'Travel to the Moon',
-            remarks:'Beautiful travel to the Moon',
-            photo: null,
-            price: 500,
-            forsale: true,
-            origin: 'Earth',
-            destination: 'Moon',
-            userId: null
+            user:'Usuario Demo',
+            email:'demo@gmail.com',
+            password: await User.hashPassword('1234'),
         },
 
     ]);
-    console.log(`***Created ${inserted.length} travels.***`)
+
+    console.log(`***Created ${inserted.length} users.***`)
+
+    // I retrieve _id value from inserted user
+    const newUserId = inserted[0]._id;
+    return newUserId;
 }
 
 async function initLocations() {
@@ -61,3 +67,45 @@ async function initLocations() {
     console.log(`***Created ${inserted.length} locations.***`)
 }
 
+async function initTravels(newUserId) {
+
+    // Deleting previous travels
+    const deleted = await Travel.deleteMany();
+    console.log(`***Deleted ${deleted.deletedCount} travels.***`);
+
+    // New travels creation
+    const inserted = await Travel.insertMany ([
+        {
+            topic:'Travel to the Moon',
+            remarks:'Beautiful travel to the Moon',
+            photo: null,
+            price: 500,
+            forsale: true,
+            origin: 'Earth',
+            destination: 'Moon',
+            userId: newUserId
+        },
+        {
+            topic:'Travel to Saturn',
+            remarks:'Beautiful travel to Saturn',
+            photo: null,
+            price: 2500,
+            forsale: true,
+            origin: 'Earth',
+            destination: 'Saturn',
+            userId: newUserId
+        },
+        {
+            topic:'Coming back from Mars',
+            remarks:'I need to come back from Mars to the Earth',
+            photo: null,
+            price: 1500,
+            forsale: false,
+            origin: 'Mars',
+            destination: 'Earth',
+            userId: newUserId
+        },
+
+    ]);
+    console.log(`***Created ${inserted.length} travels.***`)
+}
