f1c50a985c127f8aabf62b299ec4cc2dcba0c6c3
Cristian Varela Casas <cristianchantada@gmail.com>
2023-09-11 12:02:48 +0200

    Router de abrir y cerrar un viaje por el propietario realizado

--- routes/api/travels.js
+++ routes/api/travels.js
@@ -12,124 +12,142 @@ const upload = multer({ dest: 'uploads/' });
 // GET /api/travels Return all travels.
 
 router.get('/', async (req, res, next) => {
-  try {
-    const filter = {};
-    const limit = parseInt(req.query.limit);
-    const skip = parseInt(req.query.skip);
-    const sort = req.query.sort;
-    const select = req.query.select;
-    let result = await Travels.list(filter, limit, skip, sort, select);
-    let data = [];
+	try {
+		const filter = {};
+		const limit = parseInt(req.query.limit);
+		const skip = parseInt(req.query.skip);
+		const sort = req.query.sort;
+		const select = req.query.select;
+		let result = await Travels.list(filter, limit, skip, sort, select);
+		let data = [];
 
-    for (let i = 0; i < result.length; i++) {
-      const user = await User.findOne({ _id: result[i].userId });
-      result[i].userName = user.user;
-    }
+		for (let i = 0; i < result.length; i++) {
+			const user = await User.findOne({ _id: result[i].userId });
+			result[i].userName = user.user;
+		}
 
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
+		res.json(result);
+	} catch (err) {
+		next(err);
+	}
 });
 
 // GET /api/travels/:id Return a travel find by id.
 
 router.get('/:id', async (req, res, next) => {
-  try {
-    const _id = req.params.id;
-    const result = await Travels.findOne({ _id: _id });
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const _id = req.params.id;
+		const result = await Travels.findOne({ _id: _id });
+		res.json(result);
+	} catch (err) {
+		next(err);
+	}
 });
 
 // POST /api/travels Create a new travel.
 
 router.post('/', uploadPhoto.single('photo'), async (req, res, next) => {
-  try {
-    const data = req.body;
-    if (req.file) {
-      data.photo = req.file.filename;
-    }
+	try {
+		const data = req.body;
+		if (req.file) {
+			data.photo = req.file.filename;
+		}
 
-    const user = await User.findOne({ _id: data.userId });
-    data.userName = user.user;
-    const travel = new Travels(data);
-    
-    const result = await travel.save();
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
+		const user = await User.findOne({ _id: data.userId });
+		data.userName = user.user;
+		const travel = new Travels(data);
+
+		const result = await travel.save();
+		res.json(result);
+	} catch (err) {
+		next(err);
+	}
 });
 
 // PUT /api/travels/:id Update a travel by id.
 
 router.put('/:id', uploadPhoto.single('photo'), async (req, res, next) => {
-  try {
-    const _id = req.params.id;
-    const data = req.body;
-    if (req.file){
-      data.photo = req.file.filename; 
-      const oldTravel = await Travels.findOne({ _id: _id });
-      if(oldTravel.photo){
-        FileSystem.unlinkSync(`public/uploads/${oldTravel.photo}`);
-      }
-    }
-    const result = await Travels.findOneAndUpdate({ _id: _id }, data, {
-      new: true,
-    });
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
-}); 
+	try {
+		const _id = req.params.id;
+		const data = req.body;
+		if (req.file) {
+			data.photo = req.file.filename;
+			const oldTravel = await Travels.findOne({ _id: _id });
+			if (oldTravel.photo) {
+				FileSystem.unlinkSync(`public/uploads/${oldTravel.photo}`);
+			}
+		}
+		const result = await Travels.findOneAndUpdate({ _id: _id }, data, {
+			new: true,
+		});
+		res.json(result);
+	} catch (err) {
+		next(err);
+	}
+});
 
 // DELETE /api/travels/:id Delete a travel by id.
 
 router.delete('/:id', async (req, res, next) => {
-  try { 
-    const _id = req.params.id;
-    
-    const travel = await Travels.findOne({ _id: _id });
+	try {
+		const _id = req.params.id;
 
-    if (travel.photo){
-      FileSystem.unlinkSync(`public/uploads/${travel.photo}`);
-    }
-    
-    await Travels.deleteOne({ _id: _id });
-    res.json("Anuncio borrado correctamente");
-  } catch (err) {
-    next(err);
-  }   
+		const travel = await Travels.findOne({ _id: _id });
+
+		if (travel.photo) {
+			FileSystem.unlinkSync(`public/uploads/${travel.photo}`);
+		}
+
+		await Travels.deleteOne({ _id: _id });
+		res.json('Anuncio borrado correctamente');
+	} catch (err) {
+		next(err);
+	}
 });
 
 // DELETE /api/travels/deletePhoto/:photoName Delete a photo by name.
 
 router.delete('/deletePhoto/:photoName', async (req, res, next) => {
-  try {
-    const photoName = req.params.photoName;
-    FileSystem.unlinkSync(`public/uploads/${photoName}`);
-    const travel = await Travels.findOneAndUpdate({ photo: photoName }, { photo: null });
-    res.json("Foto borrada correctamente");
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const photoName = req.params.photoName;
+		FileSystem.unlinkSync(`public/uploads/${photoName}`);
+		const travel = await Travels.findOneAndUpdate(
+			{ photo: photoName },
+			{ photo: null }
+		);
+		res.json('Foto borrada correctamente');
+	} catch (err) {
+		next(err);
+	}
 });
 
 // PUT /api/travels/buy/:id buy a travel by id.
 
-router.put("/buy/:id", async (req, res, next) => {
+router.put('/buy/:id', async (req, res, next) => {
+	try {
+		const _id = req.params.id;
+		const userBuyer = req.body;
+		update = { active: false, userBuyer: userBuyer.userBuyer };
+		const result = await Travels.findOneAndUpdate({ _id: _id }, update, {
+      new: true,
+		});
+		res.json(result);
+		console.log('result', result);
+	} catch (err) {
+    next(err);
+	}
+});
+
+// PUT /api/travels/active/:id active a travel by id.
+
+router.put('/active/:id', async (req, res, next) => {
   try {
-    const _id = req.params.id;
-    const userBuyer = req.body;
-    console.log("req.body", req.body);
-    update = { active: false, userBuyer: userBuyer.userBuyer};
-    console.log("update", update);
-    const result = await Travels.findOneAndUpdate({ _id: _id }, update ,{new: true});
+    const id = req.params.id;
+    const travelActive = req.body.travelActive;
+    const result = await Travels.findOneAndUpdate({ _id: id }, {active: travelActive}, {
+      new: true,
+    });
     res.json(result);
-    console.log("result", result);
   } catch (err) {
     next(err);
   }
@@ -137,30 +155,26 @@ router.put("/buy/:id", async (req, res, next) => {
 
 // POST /api/users/:user Return a travels find by user.
 
-router.post('/users',
-upload.array('files'),
+router.post(
+	'/users',
+	upload.array('files'),
 
-async function (req, res, next) {
-  try {
+	async function (req, res, next) {
+		try {
+			const user = req.body.user;
 
-    const user = req.body.user;
-    
-    const userData = await User.findOne({ user: user });
-    const userId = userData._id;
-    const travels = await Travels.find({ userId: userId });
-     
-    res.json({ status:"OK", result: travels });
-    return
-  } catch (error) {
-      
-    res.json({ status: 400, message : "User Not Exist"  }); 
-    return   
-  }
-}
+			const userData = await User.findOne({ user: user });
+			const userId = userData._id;
+			const travels = await Travels.find({ userId: userId });
 
+			res.json({ status: 'OK', result: travels });
+			return;
+		} catch (error) {
+			res.json({ status: 400, message: 'User Not Exist' });
+			return;
+		}
+	}
 );
-
-
 
 
 module.exports = router;
