5fbf9a703ce625751e46f3f25e075554206211ca
Jesus martin romero <jesusmartinromero10@gmail.com>
2023-09-13 13:55:15 +0200

    seguimos

--- controller/passwordLinkController.js
+++ controller/passwordLinkController.js
@@ -11,103 +11,7 @@ class PasswordLinkController {
     res.locals.email = '';
     res.render('password');
   }
-  async putAPI(req, res, next) {
-    try {
-      const { email } = req.body;
-      console.log('email', email)
-
-      // buscar el usuario en la BD
-      const usuario = await Usuario.findOne({ email: email });
-      //const usuario = await Usuario.findOneAndUpdate(
-        //{ email: email },
-        //{ password: await User.hashPassword(passw) }
-      //);
-
-      // si no lo encuentro o no coincide la contraseña --> error
-      if (!usuario) {
-        res.json({
-          status: 400,
-          error: 'No existe ese email en la base de datos',
-        });
-
-        return;
-      }
-
-      // si existe mandar crear token y el email
-
-      const token = await jwt.sign(
-        { _id: usuario._id, email: usuario.email, userName: usuario.user},
-        process.env.JWT_SECRET,
-        {
-          expiresIn: '1s',
-        }
-
-      );
-      
-      
-
-
-    
-
-      
-      const port = 3000;
-      
-      // Configura nodemailer para enviar correos electrónicos
-      
-    
-      
-        // Construye la URL completa con el token
-        const fullURL = `http://localhost:${port}/updateUser/${token}`;
-        console.log('completa', fullURL)
-
-
-      // Configura el transporte de correo
-      const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-          user: process.env.EMAIL_PASSWORD,    
-          pass: process.env.PASSWOR_REMEMBER  
-        },
-      });
-
-      // Detalles del correo electrónico
-      const mailOptions = {
-        from: process.env.EMAIL_PASSWORD,
-        to: email,
-        subject: 'Recuperacion Password',
-        text: `Le escribimos de la App Space Jump con su nueva contraseña es :  ${fullURL}`
-        //passw,
-      };
-
-      // Envía el correo electrónico
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log('Error al enviar el correo:', error);
-          res.json({ error:error, msg:'Correo electrónico no enviado' })
-        } else {
-          console.log('Correo electrónico enviado:', info.response);
-          res.json({ status:200, msg:'Correo electrónico enviado correctamente' });
-
-        }
-      });
-      res.json({ jwt: token, _id: usuario._id, email: usuario.email, userName: usuario.user });
-
-      //usuario = {password:'1111'}
-
-      //   const token = await jwt.sign(
-      //     { _id: usuario._id },
-      //     process.env.JWT_SECRET,
-      //     {
-      //       expiresIn: '2d',
-      //     }
-      //   );
-
-      //   res.json({ jwt: token, userId: usuario._id });
-    } catch (err) {
-      console.log(err)
-      next(err);
-    }
-  }
+  
   async getAPI(req, res, next){
     try{
       // Captura el token de la URL
@@ -116,20 +20,7 @@ class PasswordLinkController {
       // Realiza la lógica que necesites con el token
       console.log('Token recibido:', token);
     
-      //res.json('Token recibido correctamente');
-    
-    
-    
-    
-
-  //   const jwt = require('jsonwebtoken');
-  //   // Captura el token de la URL
-  // const token = req
-  // ;
-
-  // // Realiza la lógica que necesites con el token
-  // console.log('Token recibido:', token);
-  //   console.log('tokennnnnn', token)
+      //res.json('Token recibido correctamente')
   
   
         // Verificar el token utilizando una clave secreta (si es un JWT)
