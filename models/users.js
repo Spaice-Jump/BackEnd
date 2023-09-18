8f2c7f0d8e5920a019600f26fe5de7dbdd17c97c
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-09-18 16:57:09 +0200

    fixed commit

--- models/users.js
+++ models/users.js
@@ -9,13 +9,6 @@ const userSchema = mongoose.Schema({
     password: String,
   });
 
-  
-// Creación de un índice sobre aquellos campos por los que se van a realizar búsquedas.
-
-userSchema.index({ user: 1 });
-userSchema.index({ email: 1 });
-
-
 //método estático
 userSchema.statics.hashPassword = function (password) {
     return bcrypt.hash(password, 7);
