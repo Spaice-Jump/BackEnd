66d1e30d10c628d1651bff58a11d3b77c63fda5a
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-09-20 10:32:28 +0200

    fixed problems with users.js when commit

--- models/users.js
+++ models/users.js
@@ -1,33 +1,27 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+
 
 //create esquema
 const userSchema = mongoose.Schema({
-	user: { type: String, unique: true },
-	email: { type: String, unique: true },
-	password: String,
-});
+    user: { type: String, unique: true },
+    email: { type: String, unique: true },
+    password: String,
+  });
 
 //método estático
 userSchema.statics.hashPassword = function (password) {
-	return bcrypt.hash(password, 7);
-};
-
-    
-// // Creación de un índice sobre aquellos campos por los que se van a realizar búsquedas.
-
-// userSchema.index({ user: 1 });
-// userSchema.index({ email: 1 });
-
-
+    return bcrypt.hash(password, 7);
+  };
 
 //método de instancia
 userSchema.methods.comparePassword = function (password) {
-	return bcrypt.compare(password, this.password);
-};
+    return bcrypt.compare(password, this.password);
+  };
+
 
 //Create model
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
 //export model
 module.exports = User;
